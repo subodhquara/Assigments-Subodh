f64ff6a4f50cd59d46189e24ae0291851413e3c8
subodhquara <128580054+subodhquara@users.noreply.github.com>
2023-04-17 12:13:15 +0530

    Add files via upload

---
+++ Assignment-7/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class App extends React.Component {
+  increment = () => {
+    this.props.dispatch({
+      type: "INCREMENT"
+    });
+  };
+
+  decrement = () => {
+    this.props.dispatch({
+      type: "DECREMENT"
+    });
+  };
+
+  render() {
+    return (
+      <div className="App mt-5">
+        <button onClick={this.increment} className="btn btn-success mr-5">
+          Increment
+        </button>
+        <button onClick={this.decrement} className="btn btn-danger">
+          Decrement
+        </button>
+        <h2 className="mt-5 display-1">{this.props.count}</h2>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    count: state.count
+  };
+};
+
+export default connect(mapStateToProps)(App);
